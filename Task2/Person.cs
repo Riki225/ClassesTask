76f9c2204f151bb840e5f44275aa582089907d3b
Riki225 <79812982+Riki225@users.noreply.github.com>
2021-06-28 19:36:17 +1000

    Task 2 in Progress

--- Task2/Person.cs
+++ Task2/Person.cs
@@ -26,10 +26,52 @@ namespace Task2
         
         public void GetHeightDifference()
         {
-            int p1 = Height;
+            int person1 = 0;
+            int person2 = 0;
+
+            int p1 = 0;
             int p2 = 0;
 
+            Console.WriteLine("Please Select A Person");
+            Console.WriteLine("(1) David");
+            Console.WriteLine("(2) Gavin");
+            Console.WriteLine("(3) Oliver");
+            person1 = int.Parse(Console.ReadLine());
             
+            if(person1 == 1)
+            {
+                p1 = David.Height;
+            }
+            else if(person1 == 2)
+            {
+                p1 = Gavin.Height;
+            }
+            else if(person1 == 3)
+            {
+                p1 = Oliver.Height;
+            }
+
+            Console.WriteLine("Please Select Another Person");
+            Console.WriteLine("(1) David");
+            Console.WriteLine("(2) Gavin");
+            Console.WriteLine("(3) Oliver");
+            person2 = int.Parse(Console.ReadLine());
+            
+            if(person2 == 1)
+            {
+                p2 = David.Height;
+            }
+            else if(person1 == 2)
+            {
+                p2 = Gavin.Height;
+            }
+            else if(person1 == 3)
+            {
+                p2 = Oliver.Height;
+            }
+
+            int HeightDiff = p1 - p2;
+            Console.WriteLine(HeightDiff);
 
             
         
